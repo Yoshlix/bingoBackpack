bf917eb58dc9fcd6decc1b3dc06cb269a10e1341
Simon <75274054+Yoshlix@users.noreply.github.com>
2026-01-03 15:25:40 +0100

    Add files via upload

---
+++ src/client/java/de/yoshlix/bingobackpack/BingoBackpackClient.java
@@ -0,0 +1,10 @@
+package de.yoshlix.bingobackpack;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BingoBackpackClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
