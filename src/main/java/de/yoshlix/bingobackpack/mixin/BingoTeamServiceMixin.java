bda5d43d9a3441e0d0c31f3453724addc75cac28
Simon <75274054+Yoshlix@users.noreply.github.com>
2026-01-09 20:04:45 +0100

    fix

--- src/main/java/de/yoshlix/bingobackpack/mixin/BingoTeamServiceMixin.java
+++ src/main/java/de/yoshlix/bingobackpack/mixin/BingoTeamServiceMixin.java
@@ -13,15 +13,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
  * dependency; the descriptor matches TeamService#joinTeam(IPlayerHandle,
  * BingoTeam) and joinSpectators(IPlayerHandle).
  */
-@Mixin(targets = "me.jfenn.bingo.common.team.TeamService")
+@Mixin(targets = "me.jfenn.bingo.common.team.TeamService", remap = false)
 public class BingoTeamServiceMixin {
 
-    @Inject(method = "joinTeam(Lme/jfenn/bingo/platform/IPlayerHandle;Lme/jfenn/bingo/common/team/BingoTeam;)V", at = @At("TAIL"))
+    @Inject(method = "joinTeam(Lme/jfenn/bingo/platform/IPlayerHandle;Lme/jfenn/bingo/common/team/BingoTeam;)V", at = @At("TAIL"), remap = false)
     private void bingoBackpack$afterJoinTeam(Object playerHandle, Object bingoTeam, CallbackInfo ci) {
         BingoIntegration.getInstance().manualSync();
     }
 
-    @Inject(method = "joinSpectators(Lme/jfenn/bingo/platform/IPlayerHandle;)V", at = @At("TAIL"))
+    @Inject(method = "joinSpectators(Lme/jfenn/bingo/platform/IPlayerHandle;)V", at = @At("TAIL"), remap = false)
     private void bingoBackpack$afterJoinSpectators(Object playerHandle, CallbackInfo ci) {
         BingoIntegration.getInstance().manualSync();
     }
